a1f0ec7a0d11246197f626a10d2e87f580d0a98a
Allan Saleh <allan@re-coded.com>
2020-06-18 08:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // Code your solution here
 
-function findMatching(drivers, str){
-  return drivers.filter(driver=>driver.name.toLowerCase()===str.toLowerCase());
+let drivers = [{ name: "Allan", age: 25 }, { name: "Mustafa", age: 22 }, { name: "MuWissam", age: 28 }, { name: "AllAn", age: 20 }]
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.name.toLowerCase() === str.toLowerCase());
 }
+console.log(findMatching(drivers, "Allan"));
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.name.startsWith(str));
+}
+
+console.log(fuzzyMatch(drivers, "Mu"));
+
+function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name === str);
+}
+
+console.log(findMatching(drivers, "Allan"));
